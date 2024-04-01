c409e641b79955b533d8f459205ffee216110b34
yoniii37 <wldus123164@gmail.com>
2024-04-01 18:12:55 +0900

    접근 제한자 추가

--- Account.java
+++ Account.java
@@ -5,7 +5,8 @@ public class Account {
 	String accountNo; // 계좌번호
 	String ownerName; // 예금주
 	int balance;	  // 잔액
-	
+	privite baladnfd  /// 접근제한자	
+
 	//생성자(기본생성자는 ctrl+space로 생성가능)
 	public Account() {
 		
